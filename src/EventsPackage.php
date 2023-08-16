28fb5f054c224bba7471312fe8a8866f24a3c6e8
Patrick Morpey <patrick@morphp.nl>
2023-08-16 16:30:19 +0200

    Updated dependencies

--- src/EventsPackage.php
+++ src/EventsPackage.php
@@ -13,8 +13,7 @@ class EventsPackage extends BasePackage
 
     public function dependencies(): array
     {
-        return $this->dependenciesByClass([
-        ]);
+        return [];
     }
 
     public function sourceDirectory(): string
