989d4bde8def2e26b435115a533eb2b7d0d82677
Patrick Morpey <patrick@morphp.nl>
2025-05-07 15:55:04 +0200

    Made ListenerManager cache safe

--- src/Listeners/ListenerManager.php
+++ src/Listeners/ListenerManager.php
@@ -17,6 +17,21 @@ readonly class ListenerManager implements ListenerProviderInterface
         private ListenerFinder $listenerFinder,
     )
     {
+        $this->initialize();
+    }
+
+    public function __serialize(): array
+    {
+        return [];
+    }
+
+    public function __unserialize(array $data): void
+    {
+        $this->initialize();
+    }
+
+    private function initialize(): void
+    {
         $this->listeners = $this->cacheManager->get()->get(
             __CLASS__,
             fn() => $this->listenerFinder->findAll()
